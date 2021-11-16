f18f89aa7adc352c5d86611701a78d0f89864de8
Goable <79634175+GoableNL@users.noreply.github.com>
2021-11-16 14:34:07 +0100

    ...

--- src/components/partners/partners.js
+++ src/components/partners/partners.js
@@ -5,7 +5,11 @@ const Partners = () =>
     <section className={style.partners}>
         <div className={'container'}>
             <div className={style.wrapper}>
-                
+                <img className={style.partner} src={'/image/partners/1.png'} height={40} />
+                <img className={style.partner} src={'/image/partners/2.png'} height={80} />
+                <img className={style.partner} src={'/image/partners/1.png'} height={40} />
+                <img className={style.partner} src={'/image/partners/1.png'} height={40} />
+                <img className={style.partner} src={'/image/partners/1.png'} height={40} />
             </div>
         </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import Article from "../components/article/article";
 import CallToAction from "../components/callToAction/callToAction";
 import Entrepreneurs from "../components/entrepreneurs/entrepreneurs";
 import Layout from "../components/layout/layout";
+import Partners from "../components/partners/partners";
+import Program from "../components/program/program";
 import Starting from "../components/starting/starting";
 import Topper from "../components/topper/topper";
 
@@ -30,8 +31,10 @@ const Page = () =>
         <Layout>
             <Starting active={starting} />
             <Topper />
+            <Program />
             <Entrepreneurs />
             <CallToAction />
+            <Partners />
         </Layout>
     );
 }
