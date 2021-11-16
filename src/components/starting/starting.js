8f34780e3b2778784ea6a348c0cf82a07951b5f0
Goable <79634175+GoableNL@users.noreply.github.com>
2021-11-16 13:05:58 +0100

    Update starting.js

--- src/components/starting/starting.js
+++ src/components/starting/starting.js
@@ -8,9 +8,9 @@ const Starting = ({ active }) =>
             (
                 <motion.div  className={style.starting} exit={{ opacity: 0 }} transition={{ ease: "easeOut" }} >
                     <motion.div className={style.round} animate={{ width: '100%', height: '100vh', borderRadius: 0 }} transition={{ ease: 'easeOut', delay: 1,  }}>
-                        <motion.h1 className={style.title} initial={{ transform: 'scale(1)' }} animate={{ transform: 'scale(0)' }} transition={{ ease: 'easeOut', delay: 1,  }}>
+                        <h1 className={style.title}>
                             Ane.
-                        </motion.h1>
+                        </h1>
                     </motion.div>
                 </motion.div>
             )
