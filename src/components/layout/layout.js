8a8f794e9f3518ea06e942af37a5c118137cde5f
Goable <79634175+GoableNL@users.noreply.github.com>
2021-11-16 13:20:40 +0100

    Update layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -10,6 +10,9 @@ const Layout = ({ children }) =>
             <title>
                 ANE. Woensdag 8 december om 16:30.
             </title>
+
+            <meta name={'description'} content={'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. '} />
+            <meta name={'author'} content={'Jonas ter Horst'} />
         </Head>
         <>
             <Header />
