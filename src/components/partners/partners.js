9f399691cf672e71f919bf82443ff14d02a57e3f
Goable <79634175+GoableNL@users.noreply.github.com>
2021-11-22 10:33:35 +0100

    ...

--- src/components/partners/partners.js
+++ src/components/partners/partners.js
@@ -7,10 +7,10 @@ const Partners = () =>
             <div className={style.wrapper}>
                 <img className={style.partner} src={'/image/partners/1.png'} alt={'Avans Hogeschool'} height={40} />
                 <img className={style.partner} src={'/image/partners/2.png'} alt={'FC Den Bosch'} height={60} />
+                <img className={style.partner} src={'/image/partners/3.jpg'} alt={'Regio Business'} height={40} />
                 <img className={style.partner} src={'/image/partners/1.png'} alt={'Avans Hogeschool'} height={40} />
                 <img className={style.partner} src={'/image/partners/2.png'} alt={'FC Den Bosch'} height={60} />
-                <img className={style.partner} src={'/image/partners/1.png'} alt={'Avans Hogeschool'} height={40} />
-                <img className={style.partner} src={'/image/partners/2.png'} alt={'FC Den Bosch'} height={60} />
+                <img className={style.partner} src={'/image/partners/3.jpg'} alt={'Regio Business'} height={40} />
             </div>
         </div>
     </section>
